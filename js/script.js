db38be75bdeb072d1e5899e39ee22bad0dde3e59
afrizaladiestya <afrizalputra11@gmail.com>
2025-07-19 07:18:03 +0700

    Add initial website structure, styles, and JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,20 @@
+showHalo();
+
+function showHalo() {
+    let userName = prompt("Please enter your name:");
+    // If the user clicks "Cancel", userName will be null
+    if (userName != '') {
+        document.getElementById('welcome-user').innerHTML = userName;
+    }
+}
+
+function validateForm() {
+    const nameInput = document.getElementById('name-input');
+
+    if (nameInput.value === '') {
+        alert('Please enter your name.');
+    } else {
+        document.getElementById('message-output').innerHTML = `Thank you, ${nameInput.value}, for your message!`;
+        nameInput.value = ''; // Clear the input field after submission
+    }
+}
